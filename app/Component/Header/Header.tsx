da5b6f151bfdf5ee40c095b3c6ef05c37f0867f9
Rakib Sheikh <sheikhrakib883@gmail.com>
2024-03-21 17:14:19 +0600

    product update

--- app/Component/Header/Header.tsx
+++ app/Component/Header/Header.tsx
@@ -8,20 +8,22 @@ const Header = () => {
         {name :"Guide",path :"guide"},
         {name :"Sponsor  this project",path:"sponsor"},      
         {name :"Blog",path:"blog"},   
-        {name :"My JSON Server",path:"myjson"},     
+        {name :"My JSON Server",path:"myjson"}, 
+        {name :"My JSON Server",path:"myjson"}, 
+        {name :"PODUCT",path:"/product"},       
     ]
     return (
-        <div className="py-6 md:py-0 ">
+        <div className="py-6 md:py-0 sticky top-0 z-40  bg-white drop-shadow-lg">
             <div className="all-content">
                 <Container className="">
                     <div className="menu-content md:flex justify-between ">
                         <div className="logo-area font-bold">
                             <Logo className=""/>
                         </div>
-                        <div className="menu-area flex gap-4">
+                        <div className="menu-area flex gap-1 md:gap-4 ">
                             {
                                 navber.map((items:any)=>
-                                      <Link className="text-[15px]" key={items.name} href={items.path}>{items.name}</Link>
+                                      <Link className="text-[13px] md:text-sm" key={items.name} href={items.path}>{items.name}</Link>
                                     )
                             }
                         </div>
---
+++ app/product/page.tsx
@@ -0,0 +1,92 @@
+import { FaCartArrowDown } from "react-icons/fa";
+import { IoGitCompare } from "react-icons/io5";
+import { FaHeart } from "react-icons/fa6";
+import { MdVisibility } from "react-icons/md";
+
+
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+import images from "@/public/faby.png"
+import Link from "next/link";
+const postgetData = async()=>{
+    const res = await fetch("http://localhost:3000/api/product");
+    if(!res.ok){
+        throw new Error ("no data fount")
+    }
+    return  res.json()
+}
+const Page = async () => {
+     const postData = await  postgetData()
+     console.log(postData)
+
+    //  const [productText,setProductText] = useState(false)
+    
+    return (
+        <div className="all-items py-4">
+             <p className="text-lg py-2 ">See our product</p>
+            <div className="mx-4 grid grid-cols-2 md:grid-cols-5 gap-6 cursor-pointer ">
+            
+              {
+                postData?.data?.map((item:any)=>(
+                  <div key={item?.id} className="main border group">
+                      <div className="main-area">
+                        <div className="image-area relative overflow-hidden ">
+                          <Image  src={images} alt="image" />
+                           <div className="icon-area flex group-hover:translate-x-0 duration-500 flex-col gap-2 absolute top-0 p-4 right-0 translate-x-14 ">
+                             <Link href=" " >
+                                  <div className="icon border-1 flex justify-center items-center bg-slate-500 border-[gray] rounded-full">
+                                      <FaCartArrowDown className=" text-white p-2 h-full w-full rounded-full"/>
+                                  </div>
+                              </Link>
+                              <Link href=" " >
+                                  <div className="icon border-1 bg-slate-500 border-[gray] rounded-full">
+                                      <IoGitCompare className="text-white p-2 h-full w-full rounded-full"/>
+                                  </div>
+                              </Link>
+                              <Link href=" " >
+                                  <div className="icon border-1 bg-slate-500 border-[gray] rounded-full">
+                                      <FaHeart className="text-white p-2 h-full w-full rounded-full"/>
+                                  </div>
+                              </Link>
+                              <Link href=" " >
+                                  <div className="icon border-1 bg-slate-500 border-[gray] rounded-full">
+                                      <MdVisibility className="text-white p-2 h-full w-full rounded-full"/>
+                                  </div>
+                              </Link>
+                           </div>
+                        </div>
+                        <p className="py-4 font-semibold">{item?.title}</p>
+                        <p className="py-2 relative ">
+                           {`${(item?.description).length > 100 ? (item?.description).slice(0, 70) : item?.description }`},
+                           <span className="underline absolute   bottom-2"> 
+                             <p className=" font-semibold cursor-pointer ">See More</p>
+                           </span>
+                        </p>  
+                      </div>
+                      <div className="prize-area flex  justify-between">
+                        <div className="prize ">
+                           <div className="all-items gap-1 flex items-center bg-slate-500 rounded-sm px-2 py-1">
+                              <div className="text-white">Order Now</div>
+                              <div className="icon">
+                                <FaCartArrowDown className="text-white "/>
+                              </div>
+                           </div>
+                        </div>
+                        <div className="prize font-semibold">
+                           {
+                            item.price
+                           } $
+                        </div>
+                      </div>
+                  </div>
+                )
+                  
+                )
+              }
+          </div>
+        </div>
+    );
+};
+
+export default Page;
